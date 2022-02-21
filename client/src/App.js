d60deed1df3a7661b01029eebf822d2cecabf446
NonamedBread <lgh778923@gmail.com>
2022-02-21 19:25:47 +0900

    tabs-template

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { AppBar, Tabs, Tab } from "@material-ui/core";
 
-function App() {
+
+const App = () => {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <AppBar position="static">
+        <Tabs value={value} onChange={handleChange} aria-label="Main Tabs">
+          <Tab label="Home" />
+          <Tab label="Send" />
+          <Tab label="Profile" />
+          <Tab label="Login" />
+          <Tab label="Sign in" />
+        </Tabs>
+      </AppBar>
+    </>
   );
-}
+};
 
 export default App;
